641bad4b8b30f49940951c9a757bce558c766b9b
GabrielPazinatto <gabriel.pazinatto@gmail.com>
2025-03-19 23:34:53 -0300

    Remove unnecessary comments

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,5 +1,3 @@
---drop table if exists urls;
---drop table if exists users;
 
 CREATE TABLE IF NOT EXISTS users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,25 +15,3 @@ CREATE TABLE IF NOT EXISTS urls (
 
   UNIQUE(short_url)
 );
-
--- INSERT INTO users (username, password) VALUES ('user1', 'password1');
--- INSERT INTO users (username, password) VALUES ('user2', 'password2');
--- INSERT INTO users (username, password) VALUES ('user3', 'password3');
--- INSERT INTO users (username, password) VALUES ('user4', 'password4');
--- INSERT INTO users (username, password) VALUES ('user5', 'password5');
--- INSERT INTO users (username, password) VALUES ('user6', 'password6');
--- INSERT INTO users (username, password) VALUES ('user7', 'password7');
--- INSERT INTO users (username, password) VALUES ('user8', 'password8');
--- INSERT INTO users (username, password) VALUES ('user9', 'password9');
--- INSERT INTO users (username, password) VALUES ('user10', 'password10');
-
--- INSERT INTO urls (owner_id, url, short_url) VALUES (1, 'http://example.com/unique1', 'http://short.ly/abc123');
--- INSERT INTO urls (owner_id, url, short_url) VALUES (1, 'http://example.com/unique2', 'http://short.ly/def456');
--- INSERT INTO urls (owner_id, url, short_url) VALUES (1, 'http://example.com/unique3', 'http://short.ly/ghi789');
--- INSERT INTO urls (owner_id, url, short_url) VALUES (1, 'http://example.com/unique4', 'http://short.ly/jkl012');
--- INSERT INTO urls (owner_id, url, short_url) VALUES (1, 'http://example.com/unique5', 'http://short.ly/mno345');
--- INSERT INTO urls (owner_id, url, short_url) VALUES (1, 'http://example.com/unique6', 'http://short.ly/pqr678');
--- INSERT INTO urls (owner_id, url, short_url) VALUES (1, 'http://example.com/unique7', 'http://short.ly/stu901');
--- INSERT INTO urls (owner_id, url, short_url) VALUES (1, 'http://example.com/unique8', 'http://short.ly/vwx234');
--- INSERT INTO urls (owner_id, url, short_url) VALUES (1, 'http://example.com/unique9', 'http://short.ly/yz5678');
--- INSERT INTO urls (owner_id, url, short_url) VALUES (1, 'http://example.com/unique10', 'http://short.ly/abc901');
