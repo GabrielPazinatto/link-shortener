ee67b55f4d4ed8f7abd0cd3f414b8258c92b85d9
GabrielPazinatto <gabrielrpazinatto@gmail.com>
2025-11-04 23:58:45 -0300

    fixed .env file not being found

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,16 @@
+
+import * as dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, '..', '.env') });
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: true,
   },
